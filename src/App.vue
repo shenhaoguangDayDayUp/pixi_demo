7892db5d886490da81c2e7928bbd7d8d28d482ff
xixixixixixixix <1264269942@qq.com>
2022-09-09 10:08:59 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app"><CanvasComponent /></div>
+</template>
+
+<script>
+import CanvasComponent from "./components/CanvasComponent";
+
+export default {
+  name: "App",
+  components: {
+    CanvasComponent
+  }
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #201d35;
+}
+</style>
---
+++ src/components/CanvasComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="stage" @click="pop"></div>
+</template>
+
+<script>
+import * as PIXI from "pixi.js";
+import anime from "animejs";
+import A from "@/assets/1.jpg";
+import B from "@/assets/2.jpg";
+export default {
+  name: "CanvasComponent",
+  data() {
+    return {
+      loader: null,
+      app: null,
+      colors: ["75F4F4", "90E0F3", "B8B3E9", "D999B9"],
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.loader = new PIXI.Loader();
+      this.loader.add(A).load(this.setUp);
+      this.loader.onProgress.add((val) => {});
+    },
+    setUp() {
+      this.app = new PIXI.Application({
+        transparent: true,
+        antialias: true,
+      });
+      this.$el.appendChild(this.app.view);
+      this.app.renderer.view.style.display = "block";
+      this.app.renderer.autoResize = true;
+      this.app.renderer.resize(window.innerWidth, window.innerHeight);
+
+      let texture =
+        PIXI.utils.TextureCache[
+          "https://uploads.codesandbox.io/uploads/user/4bd37c15-6b32-410f-8bcf-8c41cf1c3a19/4k4V-1.jpg"
+        ];
+      let sprite = new PIXI.Sprite(texture);
+      //  let sprite = new PIXI.Sprite.from(this.imageList[10]);
+      sprite.position.set(100, 100);
+      sprite.anchor.set(0.5);
+      console.log(this.loader.resources);
+      this.app.stage.addChild(sprite);
+    },
+    pop(event) {
+      let mouse_x = event.x,
+        mouse_y = event.y,
+        particles = [],
+        spread = 100;
+
+      for (let i = 0; i < 50; i++) {
+        let particle = new PIXI.Graphics();
+        let rand = anime.random(1, this.colors.length);
+        particle.beginFill("0x" + this.colors[rand - 1]);
+        particle.drawCircle(0, 0, 4);
+        particle.endFill();
+        particle.x = mouse_x;
+        particle.y = mouse_y;
+        particles.push(particle);
+        this.app.stage.addChild(particle);
+      }
+
+      anime({
+        targets: particles,
+        x() {
+          return anime.random(mouse_x - spread, mouse_x + spread);
+        },
+        y() {
+          return anime.random(mouse_y - spread, mouse_y + spread);
+        },
+        alpha: { delay: 300, duration: 500, value: 0 },
+        easing: "easeOutQuint",
+        complete: () => {
+          for (let particle of particles) {
+            this.app.stage.removeChild(particle);
+          }
+        },
+      });
+    },
+  },
+};
+</script>
